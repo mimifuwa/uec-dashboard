a2dfe2466411fae1afe38a005cbb1686cee72106
Mimi 1008 <mimi1008.tech@gmail.com>
2023-04-12 22:06:13 +0900

    Update dark mode

--- script/script.js
+++ script/script.js
@@ -1,7 +1,24 @@
+
+
 function clearForm(){
   let form = document.getElementById('search_input');
   let v = form.value;
   window.open('https://www.uec.ac.jp/search/?q=' + v);
   form.value = '';
   return false;
+}
+
+function changeTheme(){
+  let btn = document.getElementById('themeBtn');
+  let theme = localStorage.getItem('theme');
+
+  if(theme === 'dark'){
+    localStorage.setItem('theme', 'light');
+    btn.innerHTML = '<i class="bi bi-brightness-high"></i>';
+  }else{
+    localStorage.setItem('theme', 'dark');
+    btn.innerHTML = '<i class="bi bi-moon"></i>';
+  }
+
+  location.reload();
 }
